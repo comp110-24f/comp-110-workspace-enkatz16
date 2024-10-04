0abeeaeec7698d405e5806fe82795469333d2212
enkatz16 <enkatz@unc.edu>
2024-10-04 12:19:36 -0400

    Adjusting utils file

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -4,16 +4,23 @@ __author__: str = "730748249"
 
 
 def all(number_list: list[str], number: int) -> bool:
+    """Matches a list to the number defined"""
     all_number: bool = True
+    if len(number_list) == 0:
+        all_number = False
+    # set it to true to start, only return false if it finds something not equal
+    # or if there are no numbers
     index: int = 0
     while index < len(number_list):
-        if list[index] != number:
+        if number_list[index] != number:
             all_number = False
         index += 1
+        # keep this outside of the conditional
     return all_number
 
 
 def max(integer_list: list[int]) -> int:
+    """Finds the max of a list of numbers"""
     if len(integer_list) == 0:
         raise ValueError("max() arg is an empty List")
     index: int = 0
@@ -26,16 +33,24 @@ def max(integer_list: list[int]) -> int:
 
 
 def is_equal(list_1: list[int], list_2: list[int]) -> bool:
+    """Determines if two lists contain the same values in the same order"""
     index: int = 0
     equal: bool = True
+    if len(list_1) != len(list_2):
+        equal = False
+        return equal
+    # end early if the list lengths aren't the same
     while index < len(list_1):
         if list_1[index] != list_2[index]:
             equal = False
         index += 1
+    # only set it equal to false, since it'll be troublesome to match 3 conditions
+    # vs just one
     return equal
 
 
 def extend(list_1: list[int], append_list: list[int]) -> None:
+    """Appends one list to the end of another"""
     index: int = 0
     while index < len(append_list):
         list_1.append(append_list[index])
